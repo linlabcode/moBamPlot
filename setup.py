fb930a3963683c4e77589ca61e7d9ec21ffd2904
monikaperez <mp22@bcm.edu>
2017-10-20 15:29:14 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,15 @@
 """A tool to process and graph bam read density at specific loci.
 """
 import os
-
-
 from setuptools import setup, find_packages
+from setuptools.command.install import install
+from subprocess import check_call
+
+class PostInstallCommand(install):
+    """Post-installation for installation mode."""
+    def run(self):
+        check_call("apt-get install plastid".split())
+        install.run(self)
 
 setup(
   name='mobamplot',
@@ -18,19 +24,18 @@ setup(
 	author_email='mp22@bcm.edu',
 
 	packages=find_packages(),
-	
-	setup_requires=["numpy",
-			"cython",
-			"pysam"],
-			
 
 	install_requires=[
+		"numpy >= 2.9.0",
+		"Pysam >= 0.8.4",
+		"Cython >= 0.22.0",
 		"h5py == 2.7.0",
 		"scipy == 0.19.0",
-		"plotly == 2.0.15",
-		"plastid == 0.4.8"
+		"plotly == 2.0.15"
 	],
 	extras_require={},
 
-	scripts=['moBamPlot/getCountVectorData.py','moBamPlot/moBamPlot.py']
+	scripts=['moBamPlot/getCountVectorData.py','moBamPlot/moBamPlot.py'],
+	
+	cmdclass={'install': PostInstallCommand}
 )
